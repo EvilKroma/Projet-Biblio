c1d8432b05efbe0bbe9caf823fd3fdc96cbc7798
Dorian Deneuchatel <deneuchateldorian205@gmail.com>
2023-12-13 11:02:15 +0100

    ajout sql

---
+++ Addons/Data/g05.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 13 déc. 2023 à 10:59
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `g05`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `auteur`
+--
+
+CREATE TABLE `auteur` (
+  `idPersonne` int(11) NOT NULL,
+  `idLivre` varchar(15) NOT NULL,
+  `idRole` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `editeur`
+--
+
+CREATE TABLE `editeur` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `editeur`
+--
+
+INSERT INTO `editeur` (`id`, `libelle`) VALUES
+(1, 'Akira Toriyama');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `genre`
+--
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `genre`
+--
+
+INSERT INTO `genre` (`id`, `libelle`) VALUES
+(1, 'Manga');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `langue`
+--
+
+CREATE TABLE `langue` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `langue`
+--
+
+INSERT INTO `langue` (`id`, `libelle`) VALUES
+(1, 'Anglais'),
+(2, 'Français'),
+(3, 'Japonais'),
+(4, 'Espagnol'),
+(5, 'Italien');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livre`
+--
+
+CREATE TABLE `livre` (
+  `isbn` varchar(15) NOT NULL,
+  `titre` varchar(500) NOT NULL,
+  `editeur` int(11) NOT NULL,
+  `annee` int(11) DEFAULT NULL,
+  `genre` int(11) DEFAULT NULL,
+  `langue` int(11) DEFAULT NULL,
+  `nbpages` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `livre`
+--
+
+INSERT INTO `livre` (`isbn`, `titre`, `editeur`, `annee`, `genre`, `langue`, `nbpages`) VALUES
+('287695205X', 'Dragon Ball, Tome 1 : Sangoku', 1, 1993, 1, 2, NULL),
+('2876952068', 'Dragon Ball, Tome 2 : Kaméhaméha', 1, 1993, 1, 2, NULL),
+('2876952076', 'Dragon Ball, Tome 3 : L\'Initiation', 1, 1993, 1, 2, NULL),
+('2876952106', 'Dragon Ball, Tome 4 : Le Tournoi', 1, 1993, 1, 2, NULL),
+('2876952114', 'Dragon Ball, Tome 5 : L\'Ultime Combat	', 1, 1994, 1, 2, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personne`
+--
+
+CREATE TABLE `personne` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(150) NOT NULL,
+  `prenom` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `personne`
+--
+
+INSERT INTO `personne` (`id`, `nom`, `prenom`) VALUES
+(1, 'Toriyama', 'Akira ');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`id`, `libelle`) VALUES
+(1, 'écrivain'),
+(2, 'éditeur');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `editeur`
+--
+ALTER TABLE `editeur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `libelle` (`libelle`);
+
+--
+-- Index pour la table `langue`
+--
+ALTER TABLE `langue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `livre`
+--
+ALTER TABLE `livre`
+  ADD PRIMARY KEY (`isbn`);
+
+--
+-- Index pour la table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `editeur`
+--
+ALTER TABLE `editeur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `langue`
+--
+ALTER TABLE `langue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `personne`
+--
+ALTER TABLE `personne`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
